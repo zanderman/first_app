ce1b5d1042fd5ae47068836019cb9497304501e5
Alexander DeRieux <acd1797@vt.edu>
2013-09-15 17:25:05 -0400

    First revision of first app.

--- KiloboltGame/src/kiloboltgame/StartingClass.java
+++ KiloboltGame/src/kiloboltgame/StartingClass.java
@@ -1,31 +1,83 @@
 package kiloboltgame;
 
 import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class StartingClass extends Applet {
+public class StartingClass extends Applet implements Runnable, KeyListener {
 
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
-		super.init();
+		setSize(800, 480);
+		setBackground(Color.BLACK);
+		setFocusable(true);
+		addKeyListener(this);
+		Frame frame = (Frame) this.getParent().getParent();
+		frame.setTitle("Q-Bot Alpha");
 	}
 
 	@Override
 	public void start() {
 		// TODO Auto-generated method stub
-		super.start();
+		Thread thread = new Thread(this);
+		thread.start();
 	}
 
 	@Override
 	public void stop() {
-		// TODO Auto-generated method stub
-		super.stop();
+
 	}
 
 	@Override
 	public void destroy() {
+
+	}
+
+	// Pertains to the "implements Runnable" call at the top.
+	// 'run' method is required for ALL threads.
+	@Override
+	public void run() {
+
+		while (true) {
+			repaint();
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch(e.getKeyCode()){
+		case KeyEvent.VK_UP:
+			break;
+		case KeyEvent.VK_DOWN:
+			break;
+		case KeyEvent.VK_LEFT:
+			break;
+		case KeyEvent.VK_RIGHT:
+			break;
+		case KeyEvent.VK_SPACE:
+			break;
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		super.destroy();
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
